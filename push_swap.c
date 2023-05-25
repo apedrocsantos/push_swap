9b3f88f43fe7ccbd5bdcf78a554853c856099e87
Antonio Pedro Da Cunha Santos <anda-cun@student.42lisboa.com>
2023-05-25 15:51:58 +0100

    ps

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: anda-cun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 17:14:41 by anda-cun          #+#    #+#             */
-/*   Updated: 2023/05/24 13:32:22 by anda-cun         ###   ########.fr       */
+/*   Updated: 2023/05/25 15:19:45 by anda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	main(int argc, char **argv)
 {
-	t_list	**stack_a;
-	t_list	**stack_b;
-	int		i;
+	int	i;
 
+	/* t_list	**stack_a; */
+	/* t_list	**stack_b; */
 	i = 0;
 	if (argc > 2)
 	{
@@ -28,7 +28,7 @@ int	main(int argc, char **argv)
 			{
 				if (!ft_isdigit(**argv))
 					return (ft_printf("Error\n"));
-				stack_a->content = *argv;
+				/* stack_a->content = *argv; */
 				(*argv)++;
 			}
 			argv++;
