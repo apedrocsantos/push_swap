2f9213f825cae0cf349d108f675e5a33a61c89ab
Pedro Santos <pedrosantosstudio@gmail.com>
2023-05-30 08:10:58 +0100

    ps

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: anda-cun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 17:14:41 by anda-cun          #+#    #+#             */
-/*   Updated: 2023/05/29 18:15:49 by anda-cun         ###   ########.fr       */
+/*   Updated: 2023/05/29 22:35:17 by anda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	print_stacks(t_list **head_a, t_list **head_b)
 	t_list	*stack_a;
 	t_list	*stack_b;
 
-	print = 0;
+	print = 1;
 	stack_a = *head_a;
 	stack_b = *head_b;
 	if (print)
@@ -43,34 +43,36 @@ int	check_a(t_list **head_a, t_list **head_b)
 {
 	t_list	*next_anode;
 	t_list	*last_anode;
+	t_list	*new_head;
 	int		p;
+	int		lstsize;
+	int		i;
 
+	i = 0;
+	new_head = *head_a;
+	lstsize = ft_lstsize(*head_a);
 	next_anode = (*head_a)->next;
 	last_anode = ft_lstlast(*head_a);
-	/* if (last_anode->content == largest || last_anode->content == smallest) */
-	/* { */
-	/* 	rotate(head_a); */
-	/* 	ft_printf("ra\n"); */
-	/* 	print_stacks(head_a, head_b); */
-	/* } */
-	p = (int)last_anode->content;
+	while (i < lstsize / 2)
+	{
+		i++;
+		new_head = new_head->next;
+	}
+	p = (int)(new_head)->content;
 	ft_printf("PA: %d\n", p);
 	if (is_ordered(*head_a))
 	{
 		if (!*head_b)
 			return (0);
-		else
-		{
-			push(head_b, head_a);
-			ft_printf("pa\n");
-			print_stacks(head_a, head_b);
-			check_b(head_a, head_b);
-		}
+		push(head_b, head_a);
+		ft_printf("pa\n");
+		print_stacks(head_a, head_b);
 	}
 	else
-		while ((int)(*head_a)->content != p)
+	{
+		while (--lstsize)
 		{
-			if ((int)(*head_a)->content >= p)
+			if ((int)(*head_a)->content >= p && lstsize > 1)
 			{
 				rotate(head_a);
 				ft_printf("ra\n");
@@ -82,15 +84,18 @@ int	check_a(t_list **head_a, t_list **head_b)
 				ft_printf("pb\n");
 				print_stacks(head_a, head_b);
 				if ((*head_b)->next
-					&& (*head_b)->content < (*head_b)->next->content)
+					&& (*head_b)->content < ft_lstlast(*head_b)->content)
 				{
-					swap(head_b);
-					ft_printf("sb\n");
+					rotate(head_b);
+					ft_printf("rb\n");
 					print_stacks(head_a, head_b);
 				}
 			}
 		}
-	check_a(head_a, head_b);
+		/* if (!is_ordered_b(*head_b)) */
+		/* check_b(head_a, head_b); */
+		/* check_b(head_a, head_b); */
+	}
 	return (0);
 }
 
@@ -98,14 +103,30 @@ int	check_b(t_list **head_a, t_list **head_b)
 {
 	t_list	*next_bnode;
 	t_list	*last_bnode;
+	t_list	*new_head;
 	int		p;
+	int		i;
+	int		lstsize;
 
+	lstsize = ft_lstsize(*head_b);
+	i = 0;
+	new_head = *head_b;
 	next_bnode = (*head_b)->next;
 	last_bnode = ft_lstlast(*head_b);
-	p = (int)last_bnode->content;
-	ft_printf("PB: %d\n", p);
-	while ((int)(*head_b)->content != p)
+	while (i < lstsize / 2)
 	{
+		i++;
+		new_head = new_head->next;
+	}
+	p = (int)(new_head)->content;
+	ft_printf("PB: %d\n", p);
+	while (--lstsize)
+	{
+		if (is_ordered_b(*head_b))
+		{
+			ft_printf("b is ordered\n");
+			break ;
+		}
 		if ((int)(*head_b)->content <= p)
 		{
 			rotate(head_b);
@@ -118,21 +139,22 @@ int	check_b(t_list **head_a, t_list **head_b)
 			ft_printf("pa\n");
 			print_stacks(head_a, head_b);
 			if ((*head_a)->next
-				&& (*head_a)->content > (*head_a)->next->content)
+				&& (*head_a)->content > ft_lstlast(*head_b)->content)
 			{
-				swap(head_a);
-				ft_printf("sa\n");
+				rotate(head_a);
+				ft_printf("ra\n");
 				print_stacks(head_a, head_b);
 			}
 		}
 	}
-	if (is_ordered(*head_b))
-		check_a(head_a, head_b);
-	else
-	{
-		/* ft_printf("unordered b\n"); */
-		check_b(head_a, head_b);
-	}
+	check_a(head_a, head_b);
+	/* if (is_ordered(*head_b)) */
+	/* 	check_a(head_a, head_b); */
+	/* else */
+	/* { */
+	/* 	/1* ft_printf("unordered b\n"); *1/ */
+	/* 	check_b(head_a, head_b); */
+	/* } */
 	return (0);
 }
 
@@ -143,13 +165,28 @@ int	is_ordered(t_list *head)
 	temp = head;
 	while (head->next)
 	{
-		if (head->content < head->next->content)
-			head = head->next;
-		else
+		if (head->content > head->next->content)
 			return (0);
+		head = head->next;
 	}
 	return (1);
 }
+
+int	is_ordered_b(t_list *head)
+{
+	t_list	*temp;
+
+	temp = head;
+	while (head->next)
+	{
+		if (head->content < head->next->content)
+			return (0);
+		head = head->next;
+	}
+	return (1);
+}
+int		g_maxi = 0;
+int		g_min = 0;
 
 int	main(int argc, char **argv)
 {
